1e3a3114bc70ebc0522a203a2ab0916f47340137
Anup Agarwal <anupagarwal@Anups-MacBook-Pro.local>
2024-02-14 23:07:04 +0530

    added UTC for private-route

--- frontend/src/__tests__/HOC/private-route.test.js
+++ frontend/src/__tests__/HOC/private-route.test.js
@@ -1,42 +1,11 @@
 import { render, screen, waitFor } from "../../test-utils/render-utils"
-import LoggedOutRoute from "../../HOC/logged-out-route"
+import PrivateRoute from "../../HOC/private-route"
 import { AuthProvider } from "../../context/auth"
 import * as Router from "react-router"
 
 describe("Test Auth context", () => {
   const Component = () => <h1>Hello world</h1>
-  it("logged-out-route compoment should render provided component when user is logged out", async () => {
-    render(
-      <AuthProvider
-        value={{
-          isUserLoggedIn: false,
-          setIsUserLoggedIn: jest.fn(),
-        }}
-      >
-        <LoggedOutRoute Component={Component} />
-      </AuthProvider>,
-    )
-    await waitFor(async () => {
-      const text = await screen.findByRole("heading", { name: "Hello world" })
-      expect(text).toBeInTheDocument()
-    })
-  })
-  it("logged-out-route compoment should render nothing when user is neither logge in nor logged out", () => {
-    render(
-      <AuthProvider
-        value={{
-          isUserLoggedIn: null,
-          setIsUserLoggedIn: jest.fn(),
-        }}
-      >
-        <LoggedOutRoute Component={Component} />
-      </AuthProvider>,
-    )
-    const text = screen.queryByRole("heading", { name: "Hello world" })
-    expect(text).not.toBeInTheDocument()
-  })
-  it("logged-out-route compoment should render provided component when user is logged in", async () => {
-    jest.spyOn(Router, "Navigate").mockImplementation(() => <h1>navigate</h1>)
+  it("private-route compoment should render provided component when user is logged in", async () => {
     render(
       <AuthProvider
         value={{
@@ -44,7 +13,38 @@ describe("Test Auth context", () => {
           setIsUserLoggedIn: jest.fn(),
         }}
       >
-        <LoggedOutRoute Component={Component} />
+        <PrivateRoute Component={Component} />
+      </AuthProvider>,
+    )
+    await waitFor(async () => {
+      const text = await screen.findByRole("heading", { name: "Hello world" })
+      expect(text).toBeInTheDocument()
+    })
+  })
+  it("private-route compoment should render nothing when user is neither logge in nor logged out", () => {
+    render(
+      <AuthProvider
+        value={{
+          isUserLoggedIn: null,
+          setIsUserLoggedIn: jest.fn(),
+        }}
+      >
+        <PrivateRoute Component={Component} />
+      </AuthProvider>,
+    )
+    const text = screen.queryByRole("heading", { name: "Hello world" })
+    expect(text).not.toBeInTheDocument()
+  })
+  it("private-route compoment should render provided component when user is logged out", async () => {
+    jest.spyOn(Router, "Navigate").mockImplementation(() => <h1>navigate</h1>)
+    render(
+      <AuthProvider
+        value={{
+          isUserLoggedIn: false,
+          setIsUserLoggedIn: jest.fn(),
+        }}
+      >
+        <PrivateRoute Component={Component} />
       </AuthProvider>,
     )
     await waitFor(async () => {
