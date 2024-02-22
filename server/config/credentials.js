e38a4115d28e42ec611f9c77ebf2c830c9c12fc1
Anup Agarwal <anupagarwal@Anups-MacBook-Pro.local>
2024-02-23 00:07:29 +0530

    bug fix

--- server/config/credentials.js
+++ server/config/credentials.js
@@ -1,5 +1,7 @@
-export const DB_USERNAME = process.env.DB_USERNAME
-export const DB_PASSWORD = process.env.DB_PASSWORD
-export const REDIS_URL = process.env.REDIS_URL
-export const SECRET_KEY = process.env.SECRET_KEY
-export const PORT = process.env.PORT
+module.exports = {
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  REDIS_URL: process.env.REDIS_URL,
+  SECRET_KEY: process.env.SECRET_KEY,
+  PORT: process.env.PORT,
+}
